0b8e6550b5e590cac6be6678279bcbebdf1f8667
Jojimbo20 <jordanrodrigues@hotmail.co.uk>
2015-12-02 19:06:33 +0000

    Added palindrome function

--- C#/String.cs
+++ C#/String.cs
@@ -21,5 +21,25 @@ namespace MyLibrary
 
             return (editedStr);
         }
+
+        static bool isPalindrome(int x)
+        {
+            //I declared two strings, one to store the unchanged number
+            //One to store the reversed number
+            string unReversed = x.ToString();
+            string Reversed = Hello.Reverse(unReversed);
+
+            //Because I can't figure out how to compare strings I'm gonna compare the ints
+            int reversedInt = Convert.ToInt32(Reversed);
+
+            //If the reversed one and the normal one are th e same it's a palindrome
+            if (x == reversedInt)
+            {
+                return (true);
+            }
+
+            else
+                return (false);
+        }
     }
 }
---
+++ README.txt
@@ -0,0 +1,4 @@
+# My-Libraries
+A list of libraries of functions I have created.
+02/12/2015 17:51
+Added my library for C#
