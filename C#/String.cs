4a6cf29a6b7708cedf5c669046c57b0692ab77e5
Jojimbo20 <jordanrodrigues@hotmail.co.uk>
2015-12-02 17:49:53 +0000

    Added my library
    Currently one function, reversing a string

---
+++ C#/String.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Hello
+    {
+        public static string Reverse(string raw)
+        {
+            //Define strings so that the user can enter a string and the computer can process the data
+            string editedStr = "";
+
+            //Where the magic happens:
+            foreach (char c in raw) //For every individual character in the string,
+            {
+                editedStr = c + editedStr; //Add the character to the editedStr
+            }
+
+            return (editedStr);
+        }
+    }
+}
